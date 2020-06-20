fa7d362d4756f5cbe576da08f13c235022e47845
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-06-20 19:51:07 +0300

    A&DS

--- code-exam/src/ModuleArithmetics.java
+++ code-exam/src/ModuleArithmetics.java
@@ -3,6 +3,35 @@ public class ModuleArithmetics {
 
     }
 
-    // GCD
-    // LCM
+    // возведение в степень в модульной арифметике
+    // по времени T = O(logb)
+    public static int pow2(int a, int b, int p) {
+        int result;
+        if (b == 1) {
+            result = a % p;
+        } else if (b % 2 == 1) {
+            result = (a % p * pow2(a, b-1, p)) % p;
+        } else {
+            int x = pow2(a, b/2, p);
+            result = (x*x) % p;
+        }
+        return result;
+    }
+
+    // GCD - greatest common divisor
+    // по времени T = O(log(max*(a,b))
+    public static int greatestDiv(int a, int b) {
+        while (b != 0) {
+            int x = a % b;
+            a = b;
+            b = x;
+        }
+        return a;
+    }
+
+    // LCM - least common multiple
+    // по времени T = O(log(max(a,b)))
+    public static int leastMult(int a, int b) {
+        return a*b / greatestDiv(a,b);
+    }
 }
---
+++ code-exam/src/StringProcessing.java
@@ -0,0 +1,44 @@
+public class StringProcessing {
+    public static void main(String[] args) {
+
+    }
+
+    // hash + fingerprinting
+    // по времени T = O(n)
+    public static boolean hashEquals(String s1, String s2) {
+        if (s1.length() != s2.length()){
+            return false;
+        }
+        long[] s1L = new long[s1.length()];
+        long[] s2L = new long[s2.length()];
+        for (int i = 0;i<s1.length();i++){
+            s1L[i] = s1.substring(0,i).hashCode();
+            s2L[i] = s2.substring(0,i).hashCode();
+        }
+        long[] pow = new long[s1.length()];
+        long[] hs1 = new long[s1.length()];
+        long[] hs2 = new long[s2.length()];
+        long p = (long) Math.pow(2,64)+ 1;
+        pow[0] = 1;
+        for (int i = 1; i < s1.length(); i++) {
+            pow[i] = pow[i - 1] * p;
+        }
+        hs1[0] = s1L[0];
+        for (int i = 1; i < s1.length(); i++) {
+            hs1[i] = hs1[i - 1] + pow[i] * s1L[i];
+        }
+        hs2[0] = s2L[0];
+        for (int i = 1; i < s2.length(); i++) {
+            hs2[i] = hs2[i - 1] + pow[i] * s2L[i];
+        }
+
+        if ((hs1[s1.length()-1])==(hs2[s2.length()-1])){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // алгоритм рабин-карпа
+    // по времени
+}
